092e646e0de57f0dbdeffba2003b1c4451ef0f3c
aviatrix <amishapatel792834@gmail.com>
2026-07-23 11:56:09 +0530

    Connect React frontend to FastApi backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,3 +21,13 @@ def root():
     return {
         "message": "API is running"
     }
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
--- frontend/src/pages/Tasks.tsx
+++ frontend/src/pages/Tasks.tsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
+
 import Layout from "../components/layout/Layout";
+import { getTasks } from "../services/taskService";
 
 function Tasks() {
-  return (
-    <Layout>
-      <h1 className="text-3xl font-bold">
-        Tasks
-      </h1>
-    </Layout>
-  );
+
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+
+        getTasks()
+            .then(setTasks)
+            .catch(console.error);
+
+    }, []);
+
+    return (
+
+        <Layout>
+
+            <h1 className="text-3xl font-bold mb-6">
+                Tasks
+            </h1>
+
+            <pre>
+
+                {JSON.stringify(tasks, null, 2)}
+
+            </pre>
+
+        </Layout>
+
+    );
+
 }
 
 export default Tasks;
